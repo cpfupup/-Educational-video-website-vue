6341751299aafd289060d6af5e15d36773fe1cfd
asherchen <asherchen@futunn.com>
2022-10-06 00:15:59 +0800

    添加了自动解析ant组件

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from "@vitejs/plugin-vue-jsx";
+// vite.config.js
+import Components from 'unplugin-vue-components/vite'
+import {
+  AntDesignVueResolver,
+  ElementPlusResolver,
+  VantResolver,
+} from 'unplugin-vue-components/resolvers'
 
+// your plugin installation
+Components({
+  resolvers: [
+    AntDesignVueResolver(),
+    ElementPlusResolver(),
+    VantResolver(),
+  ],
+})
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueJsx()]
+  plugins: [vue(), vueJsx()]
 })
