d911926980c53a690f1a455a637a674ae62defde
asherchen <asherchen@futunn.com>
2022-10-08 21:04:42 +0800

    更改menu

--- vite-project/src/module/menu/menu.tsx
+++ vite-project/src/module/menu/menu.tsx
@@ -1,36 +1,29 @@
-import { defineComponent, reactive } from "vue"
-import { Menu,MenuItem } from 'ant-design-vue';
+import { defineComponent, ref } from "vue"
+import { Menu, MenuItem } from 'ant-design-vue';
 // import { defineComponent, onUnmounted, reactive } from 'vue'
 import { storeToRefs } from 'pinia'
 
 export const Menus = defineComponent({
-    setup() {
-        return () => {
-            return (
-                <Menu>
-                    <MenuItem>1</MenuItem>
-                    <MenuItem>2</MenuItem>
-                    <MenuItem>3</MenuItem>
-                    <MenuItem>4</MenuItem>
-                    <MenuItem>5</MenuItem>
-                    <MenuItem>6</MenuItem>
-                    <MenuItem>1</MenuItem>
-                    <MenuItem>2</MenuItem>
-                    <MenuItem>3</MenuItem>
-                    <MenuItem>4</MenuItem>
-                    <MenuItem>5</MenuItem>
-                    <MenuItem>6</MenuItem>
-                    <MenuItem>1</MenuItem>
-                    <MenuItem>2</MenuItem>
-                    <MenuItem>3</MenuItem>
-                    <MenuItem>4</MenuItem>
-                    <MenuItem>5</MenuItem>
-                    <MenuItem>6</MenuItem>
-
-                </Menu>
-            )
-
-        }
+  setup() {
+    const menus = ref([
+      {
+        name: 'New Issues',
+        id: 'New Issues',
+        childrenVisible: true,
+        children: [
+          {
+            name: 'IPO Subscription List',
+            id: 'IPO Subscription List',
+          },
+        ],
+      },
+    ])
+    return () => {
+      return (
+          <Menu mode={menus.value} activeId="IPO Subscription List" />
+      )
 
     }
-})
+
+
+  })
