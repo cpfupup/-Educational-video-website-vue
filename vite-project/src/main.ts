8a5007e5a639d8126b6362d4b75719fbc65bc9f3
asherchen <asherchen@futunn.com>
2022-10-16 19:20:25 +0800

    安装pinia

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
 import App from './App'
 import router from "./router"
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia).mount("#app");
 
