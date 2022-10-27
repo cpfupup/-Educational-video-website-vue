c089f688539823e8b67e41d9830d01b1f4596349
asherchen(陈芃霏) <asherchen@futunn.com>
2022-10-27 17:24:06 +0800

    更改为导入tsx文件

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,5 @@
 import { defineComponent } from 'vue'
-import { MenuSetting } from './components/menu/menu'
+import { MenuSetting } from './components/menu/menu.tsx'
 import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
 
 export default defineComponent({
