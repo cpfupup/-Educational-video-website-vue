08cac40c23ca4c10176f5f2ba348966d160eb4d5
asherchen(陈芃霏) <asherchen@futunn.com>
2022-10-28 17:28:42 +0800

    引入轮播图测试，后续需要更改

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,13 @@
 import { defineComponent } from 'vue'
 import { MenuSetting } from './components/menu/menu.tsx'
 import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
-
+import { HomeSetting } from './module/home-setting/home-setting-component.tsx';
 export default defineComponent({
   setup() {
     return () => (
       <div>
         <MenuSetting />
+        <HomeSetting/>
       </div>
     )
   }
