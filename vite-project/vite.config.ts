50752c48283e03344b5f468e7288a9d2dc0c1441
asherchen(陈芃霏) <asherchen@futunn.com>
2022-11-16 17:20:36 +0800

    去除多余代码

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
-// https://vitejs.dev/config/
+
 import Components from 'unplugin-vue-components/vite'
 import {
   AntDesignVueResolver,
