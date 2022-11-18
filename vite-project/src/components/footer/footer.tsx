0e0d8d9e96f0b85df8a1d0fd95ef3601cd732c54
asherchen(陈芃霏) <asherchen@futunn.com>
2022-11-18 18:40:24 +0800

    尝试添加footer

--- vite-project/src/components/footer/footer.tsx
+++ vite-project/src/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-import { Button } from "ant-design-vue";
+import { Descriptions } from "ant-design-vue";
 import { defineComponent, onBeforeMount, ref } from "vue";
 import style from './footer.module.css'
 export const FooterSetting = defineComponent({
@@ -8,9 +8,15 @@ export const FooterSetting = defineComponent({
       return (
         <div class={style.backGround}>
           {/* 中间实体盒子 */}
-          <div class={style.header}>
-            
-          </div>
+          <Descriptions  class={style.footer} >
+            <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+            <Descriptions.Item label="Telephone" class={style.itemFont}>1810000000</Descriptions.Item>
+            <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
+            <Descriptions.Item label="Remark">empty</Descriptions.Item>
+            <Descriptions.Item label="Address">
+              No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+            </Descriptions.Item>
+          </Descriptions>
         </div>
       )
     }
